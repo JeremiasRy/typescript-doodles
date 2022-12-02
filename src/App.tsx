aac16afba49fc599eb7df661ed6aa1fd24240067
JeremiasRy <ryttari.jeremias@gmail.com>
2022-12-02 11:35:06 +0200

    refactor: changed order of components

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,10 @@ function App() {
   return (
     <>
     <div className="component-wrapper">
-      <Game />
+      <GameOfLife />
     </div>
     <div className="component-wrapper">
-      <GameOfLife />
+      <Game />
     </div>
     <div className="component-wrapper">
       <Counter />
